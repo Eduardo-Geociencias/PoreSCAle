6d5cd066bf0087d4c665e59fd11e15eab35c88d5
Eduardo-Geociencias <106560403+Eduardo-Geociencias@users.noreply.github.com>
2022-11-17 18:44:18 -0600

    Add files via upload

---
+++ graphics.py
@@ -0,0 +1,6 @@
+from PoreSCAle import*
+
+# Creamos 10 imágenes de radio = 25, estos parámetros se pueden modificar
+for i in range(10):
+    #parámetros PoreSCAle(radio, i)
+    mass,matrix=PoreSCAle(25, i)
